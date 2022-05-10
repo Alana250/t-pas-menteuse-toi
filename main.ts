aad5eb3a339bf40487f82c49e31de2b665b0c04f
Alana250 <103495599+Alana250@users.noreply.github.com>
2022-05-10 11:02:04 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,20 @@
 let n = 0
-while (!(input.buttonIsPressed(Button.A))) {
-    n = 1
+while (!(input.buttonIsPressed(Button.B))) {
+    if (input.buttonIsPressed(Button.A)) {
+        n += 1
+        if (n > 3) {
+            n = 1
+        }
+        if (n == 1) {
+            basic.showIcon(IconNames.Happy)
+        } else {
+            if (n == 2) {
+                basic.showIcon(IconNames.Asleep)
+            } else {
+                basic.showIcon(IconNames.Sad)
+            }
+        }
+    }
 }
+basic.showString("votre humeur")
+basic.showNumber(n)
